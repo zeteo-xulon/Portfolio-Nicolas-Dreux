5931cd1cf2b3a0670b5be4fa85446d308fccf105
zeteo_xulon <n.dreux@live.fr>
2022-08-10 17:24:23 +0200

    first commit

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import portrait from '../profil_picND.png';
+
+export default function Home(){
+  return(
+    <main className="home">
+      <img src={portrait} className="user" alt="user" />
+      
+    </main> 
+  )
+}
